ece9bcb0f026dd8c53b65b6cf9ca0d748c6f105a
Cristhian Rosas <cristhianrosas159@hotmail.com>
2023-09-03 03:55:40 -0500

    add label in movie-card__body

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -22,7 +22,9 @@ onMounted(async () => {
 
 const getGenres = (genreIds) => {
   const genres = genreIds.map((genreId) => {
-    const genre = genders.value && genders.value.genres.find((genre) => genre.id === genreId);
+    const genre =
+      genders.value &&
+      genders.value.genres.find((genre) => genre.id === genreId);
     return genre ? genre.name : "";
   });
   return genres.join(", ");
@@ -37,16 +39,25 @@ const getGenres = (genreIds) => {
       <div v-if="moviesData && moviesData.results">
         <!-- Contenido que se mostrará si moviesData.results existe -->
         <div class="movie-card-container">
-          <div class="movie-card" v-for="movie in moviesData.results" :key="movie.id">
+          <div
+            class="movie-card"
+            v-for="movie in moviesData.results"
+            :key="movie.id"
+          >
             <img
               :src="'https://image.tmdb.org/t/p/w500' + movie.poster_path"
               :alt="movie.original_title"
               class="movie-card__img"
             />
-            <div class="movie-card__body">
-              <h2 class="movie-card__title">{{ movie.original_title }}</h2>
-              <h5>{{ getGenres(movie.genre_ids) }}</h5>
-              <h4> ❤️ {{ movie.vote_average }}</h4>
+            <div>
+              <div class="movie-card__body">
+                <h2 class="movie-card__title">{{ movie.original_title }}</h2>
+                <h5>{{ getGenres(movie.genre_ids) }}</h5>
+                <h4>❤️ {{ movie.vote_average }}</h4>
+              </div>
+              <div class="movie-card__title__label">
+                <b class="label-icon">+</b>
+              </div>
             </div>
           </div>
         </div>
