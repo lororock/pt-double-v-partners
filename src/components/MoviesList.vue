73d337ae6034370fb7cbec87de2fa6b9a98a4789
Cristhian Rosas <cristhianrosas159@hotmail.com>
2023-09-04 12:02:34 -0500

    filter in moviesList

--- src/components/MoviesList.vue
+++ src/components/MoviesList.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { ref, onMounted } from "vue";
 import { getMovies, getGenders } from "../services/getData.js";
-import Modal from "./Modal.vue"
+import Modal from "./Modal.vue";
 
 const moviesData = ref(null);
 const genders = ref(null);
@@ -38,6 +38,48 @@ const getGenres = (genreIds) => {
 
 <template>
   <div>
+    <div class="filters">
+      <div class="filter-contain">
+        <div class="filter-select">IN THEATERS</div>
+        <div>COMING SOON</div>
+        <div>CHARTS</div>
+        <div>TV SERIES</div>
+        <div>TRAILERS</div>
+        <div class="filter-more">MORE ↓</div>
+        <div class="filter-score">⭐ 179</div>
+      </div>
+      <div class="filter-ranking">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          x="0px"
+          y="0px"
+          width="30"
+          height="30"
+          viewBox="0 0 50 50"
+        >
+          <path
+            d="M 3 9 A 1.0001 1.0001 0 1 0 3 11 L 47 11 A 1.0001 1.0001 0 1 0 47 9 L 3 9 z M 3 24 A 1.0001 1.0001 0 1 0 3 26 L 47 26 A 1.0001 1.0001 0 1 0 47 24 L 3 24 z M 3 39 A 1.0001 1.0001 0 1 0 3 41 L 47 41 A 1.0001 1.0001 0 1 0 47 39 L 3 39 z"
+          ></path>
+        </svg>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="30"
+          height="30"
+          viewBox="0 0 100 100"
+        >
+          <circle cx="25" cy="25" r="5" fill="#000" />
+          <circle cx="50" cy="25" r="5" fill="#000" />
+          <circle cx="75" cy="25" r="5" fill="#000" />
+          <circle cx="25" cy="50" r="5" fill="#000" />
+          <circle cx="50" cy="50" r="5" fill="#000" />
+          <circle cx="75" cy="50" r="5" fill="#000" />
+          <circle cx="25" cy="75" r="5" fill="#000" />
+          <circle cx="50" cy="75" r="5" fill="#000" />
+          <circle cx="75" cy="75" r="5" fill="#000" />
+        </svg>
+        <input type="range" >
+      </div>
+    </div>
     <div class="card">
       <div v-if="moviesData && moviesData.results">
         <div class="movie-card-container">
