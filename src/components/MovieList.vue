0553aaf78b8ca01b5a6224eab2a32dc68462796f
Cristhian Rosas <cristhianrosas159@hotmail.com>
2023-09-04 00:22:33 -0500

    responsive modal

--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -90,14 +90,59 @@ const getGenres = (genreIds) => {
                 "
                 class="movie-card__img"
               />
+              <img
+                :src="
+                  'https://image.tmdb.org/t/p/w500' + selectedMovie.backdrop_path
+                "
+                class="movie-card__img2"
+              />
             </div>
             <div class="modal-content__right">
-              <h2>
-                {{
-                  selectedMovie ? selectedMovie.original_title : "no funciono"
-                }}
-                <p>Contenido del modal...</p>
-              </h2>
+              <div class="content-right__top">
+                <div class="content-right__info">
+                <h2>
+                  {{
+                    selectedMovie ? selectedMovie.original_title : "no funciono"
+                  }}
+                </h2>
+                <div class="right-info__movie">
+                  <b>{{ getGenres(selectedMovie.genre_ids) }}</b>
+                  <div>
+                    <p>{{ selectedMovie.adult ? "M18" : "PG-13" }}</p>
+                    <p>{{ selectedMovie.release_date }}</p>
+                  </div>
+                </div>
+                <div class="right-info__details">
+                  <div>
+                    <div class="detail">
+                      ❤️ {{ selectedMovie.vote_average }}
+                      <h3>rating</h3>
+                    </div>
+                  </div>
+                  <div>
+                    <div class="detail">
+                      👎 {{ Math.floor(Math.random() * (100 - 40 + 1)) + 40 }}
+                      <h3>score</h3>
+                    </div>
+                  </div>
+                  <div>
+                    <div class="detail">
+                      🏆 {{ Math.floor(Math.random() * (20 - 3 + 1)) + 3 }}
+                      <h3>awards</h3>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <p>
+                {{ selectedMovie.overview }}
+              </p>
+              <p class="full-movie">FULL MOVIE PROFILE →</p>
+              </div>
+              
+              <div class="btns">
+                <div class="btnl">Watch Trailer ▶</div>
+                <div class="btnr">To Wathclist ⭐</div>
+              </div>
             </div>
           </div>
         </div>
