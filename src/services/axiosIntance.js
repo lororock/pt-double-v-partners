cc94210dcfd537406d922a4a16b069be643d149d
Cristhian Rosas <cristhianrosas159@hotmail.com>
2023-09-22 07:46:03 -0500

    delete console log

--- src/services/axiosIntance.js
+++ src/services/axiosIntance.js
@@ -3,7 +3,6 @@ import axios from "axios";
 const apiKey = import.meta.env.VITE_APP_API_KEY
 const bearerToken = import.meta.env.VITE_APP_BEARER_TOKEN
 
-console.log('hola');
 
 export const baseURL = 'https://api.themoviedb.org/3/';
 
